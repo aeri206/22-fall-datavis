a7480840b72d72b4d59ab301dddef03eced5b722
Jiwon Song <sjw_512@naver.com>
2022-12-05 15:51:05 +0900

    scatter 1차완성

--- vis/src/Scatter.js
+++ vis/src/Scatter.js
@@ -1,80 +1,132 @@
-import { Vega } from 'react-vega';
+import { VegaLite } from 'react-vega';
 
 const spec = {
-  "$schema": "https://vega.github.io/schema/vega/v5.json",
-  "description": "A basic scatter plot example depicting automobile statistics.",
-  "width": 400,
-  "height": 400,
-  "padding": 5,
-  "data": [
-    {
-      "name": "source", 
-      "url": "http://localhost:8080/scatter/data_final.tsv",
-      "format": {"type": "tsv", "parse": "auto"},
-    }
+  "config": {"view": {"continuousWidth": 500, "continuousHeight": 500}},
+  "data": {"url": "http://localhost:8080/scatter/data_final.tsv"},
+  "usermeta": [
+    "country",
+    "continent",
+    "COVID cases/100k",
+    "capital city latitude",
+    "GDP real growth rate",
+    "unemployment rate",
+    "gini index",
+    "inflation rate",
+    "median age",
+    "total fertility rate",
+    "life expectancy",
+    "HDI index",
+    "GDP per capita",
+    "happiness score",
+    "corruption index(cpi)",
+    "abstainer(lifetime)",
+    "abstainer(12 month)",
+    "consumer(12 month)",
+    "consumer gender ratio(male/female)",
+    "heavy episodic drinking(30 days)",
+    "alcohol attributable death",
+    "years of life lost by alcohol",
+    "gender gap index",
+    "work hours (unpaid+paid+study)",
+    "alcohol consumption",
+    "alcohol consumption(drinkers only)"
   ],
-  "scales": [
+  "params": [
     {
       "name": "x",
-      "type": "linear",
-      "round": true,
-      "nice": true,
-      "zero": true,
-      "domain": {"data": "source", "field": "happiness"},
-      "range": "width"
+      "value": "COVID cases/100k",
+      "bind": {
+        "input": "select",
+        "options": [
+           "COVID cases/100k",
+            "capital city latitude",
+            "GDP real growth rate",
+            "unemployment rate",
+            "gini index",
+            "inflation rate",
+            "median age",
+            "total fertility rate",
+            "life expectancy",
+            "HDI index",
+            "GDP per capita",
+            "happiness score",
+            "corruption index(cpi)",
+            "abstainer(lifetime)",
+            "abstainer(12 month)",
+            "consumer(12 month)",
+            "consumer gender ratio(male/female)",
+            "heavy episodic drinking(30 days)",
+            "alcohol attributable death",
+            "years of life lost by alcohol",
+            "gender gap index",
+            "work hours (unpaid+paid+study)",
+            "alcohol consumption",
+            "alcohol consumption(drinkers only)"
+        ]
+      }
     },
     {
       "name": "y",
-      "type": "linear",
-      "round": true,
-      "nice": true,
-      "zero": true,
-      "domain": {"data": "source", "field": "consumption"},
-      "range": "height"
-    },
-  ],
-  "axes": [
-    {
-      "scale": "x",
-      "grid": true,
-      "domain": false,
-      "orient": "bottom",
-      "tickCount": 5,
-      "title": "happiness score"
-    },
-    {
-      "scale": "y",
-      "grid": true,
-      "domain": false,
-      "orient": "left",
-      "titlePadding": 5,
-      "title": "alcohol consumption"
-    }
-  ],
-  "marks": [
-    {
-      "name": "marks",
-      "type": "symbol",
-      "from": {"data": "source"},
-      "encode": {
-        "update": {
-          "x": {"scale": "x", "field": "happiness score"},
-          "y": {"scale": "y", "field": "alcohol consumption"},
-          "shape": {"value": "circle"},
-          "strokeWidth": {"value": 2},
-          "opacity": {"value": 0.5},
-          "stroke": {"value": "#4682b4"},
-          "fill": {"scale": "color", "field":"continent"}
-        }
+      "value": "abstainer(lifetime)",
+      "bind": {
+        "input": "select",
+        "options": [
+          "abstainer(lifetime)",
+          "abstainer(12 month)",
+          "consumer(12 month)",
+          "consumer gender ratio(male/female)",
+          "heavy episodic drinking(30 days)",
+          "alcohol attributable death",
+          "years of life lost by alcohol",
+          "alcohol consumption",
+          "alcohol consumption(drinkers only)"
+        ]
       }
     }
-  ]}
+  ],
+  "transform": [
+    {"calculate": "datum[x]", "as": "xcalc"},
+    {"calculate": "datum[y]", "as": "ycalc"}
+  ],
+  "hconcat": [
+    {
+      "vconcat": [
+        {
+          "mark": "point",
+          "params": [{"name": "brush", "select": "interval"}],
+          "encoding": {
+            "color": {
+              "field": "continent",
+              "type": "nominal",
+            },
+            "tooltip": [
+              {"field": "country", "type": "nominal"},
+              {"field": "continent", "type": "nominal"},
+              {"field": "xcalc", "type": "quantitative", "title": "x"},
+              {"field": "ycalc", "type": "quantitative", "title": "y"}
+            ],
+            "x": {
+              "field": "xcalc",
+              "type": "quantitative",
+              "axis": {"title": {"expr": "x"}}
+            },
+            "y": {
+              "field": "ycalc",
+              "type": "quantitative",
+              "axis": {"title": {"expr": "y"}}
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
 
 function Scatter() {
     return (
       <div className="Scatter">
         <h1>Scatter</h1>
-        <Vega spec={spec}/>
+        <VegaLite spec={spec}/>
       </div>
     );
   }
