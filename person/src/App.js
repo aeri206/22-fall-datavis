18be086223724897667aa9ad8a6d0baef907cc81
Sodaking <joshua2000@naver.com>
2022-12-06 13:42:08 +0900

    Hide Scroll

--- person/src/App.js
+++ person/src/App.js
@@ -17,25 +17,27 @@ const store = createStore(reducers)
 function App() {
   return (
     <Provider store={store}>
-      <Nav direction="row" background="brand" height="55px" pad="xxsmall" style={{paddingLeft: 30}} >
-        <Heading level={4} margin="small">Drink Expedition</Heading>
-        <Anchor href="http://gpu.hcil.snu.ac.kr:8888/" icon={<Map />} style={{marginLeft: 'auto', marginRight: '15px'}}/>
-        {/* <Anchor href="http://gpu.hcil.snu.ac.kr:8890/" icon={<User />} style={{marginLeft: 'auto', marginRight: '25px'}}/> */}
-    </Nav>  
-      <div className="App">
-        <div className="grid grid-rows-2 grid-cols-2 h-screen w-screen py-3 px-6 gap-5 bg-slate-300">
-        <Paper elevation={3}>
-          <AboutMe/>
-        </Paper>
-        <Paper elevation={3}>
-          <LineChart/>
-        </Paper>
-        <Paper elevation={3}>
-          <Scatter />
-        </Paper>
-        <Paper elevation={3}>
-          <ScatterPlot/>
-        </Paper>
+      <div className='overflow-y-hidden h-screen'>
+          <Nav direction="row" background="brand" height="55px" pad="xxsmall" style={{paddingLeft: 30}} >
+            <Heading level={4} margin="small">Drink Expedition</Heading>
+            <Anchor href="http://gpu.hcil.snu.ac.kr:8888/" icon={<Map />} style={{marginLeft: 'auto', marginRight: '15px'}}/>
+            {/* <Anchor href="http://gpu.hcil.snu.ac.kr:8890/" icon={<User />} style={{marginLeft: 'auto', marginRight: '25px'}}/> */}
+          </Nav>  
+        <div className="App h-full">
+          <div className="grid grid-rows-2 grid-cols-2 h-full w-screen pt-3 px-6 gap-5 bg-slate-300 pb-16">
+          <Paper elevation={3}>
+            <AboutMe/>
+          </Paper>
+          <Paper elevation={3}>
+            <LineChart/>
+          </Paper>
+          <Paper elevation={3}>
+            <Scatter />
+          </Paper>
+          <Paper elevation={3}>
+            <ScatterPlot/>
+          </Paper>
+          </div>
         </div>
       </div>
     </Provider>
